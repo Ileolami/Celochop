4611c57569ff393612bfe73a71878f4c5f0ba906
Ileolami <99335756+Ileolami@users.noreply.github.com>
2024-07-14 19:44:16 +0100

    chore: Update styles for Navbar and Search components

--- src/Features/Navbar.jsx
+++ src/Features/Navbar.jsx
@@ -19,16 +19,16 @@ const Navbar = () => {
       <nav>
         <ul className="flex flex-col p-4">
           <li className="mb-4">
-            <Link to='/homepage' className="hover:bg-[#181C2E] border-none py-2 px-4 rounded-xl tracking-wide">Home</Link>
+            <Link to='/homepage' className="hover:bg-[#181C2E] hover:bg-opacity-25 border-none py-2 px-4 rounded-xl tracking-wide">Home</Link>
           </li>
           <li className="mb-4">
-            <Link to='/profile' className="hover:bg-[#181C2E] border-none py-2 px-4 rounded-xl tracking-wide">Profile</Link>
+            <Link to='/profile' className="hover:bg-[#181C2E] hover:bg-opacity-25 border-none py-2 px-4 rounded-xl tracking-wide">Profile</Link>
           </li>
           <li className="mb-4">
-            <Link to='/additem' className="hover:bg-[#181C2E] border-none py-2 px-4 rounded-xl tracking-wide">Add Item</Link>
+            <Link to='/additem' className="hover:bg-[#181C2E] hover:bg-opacity-25 border-none py-2 px-4 rounded-xl tracking-wide">Add Item</Link>
           </li> 
           <li className="mb-4">
-            <Link to='/search' className="hover:bg-[#181C2E] border-none py-2 px-4 rounded-xl tracking-wide">Search</Link>
+            <Link to='/search' className="hover:bg-[#181C2E] hover:bg-opacity-25 border-none py-2 px-4 rounded-xl tracking-wide">Search</Link>
           </li>   
         </ul>
       </nav>
--- src/homepage/Search.jsx
+++ src/homepage/Search.jsx
@@ -5,16 +5,16 @@ const Search = () => {
   return (
     <div className="my-6">
       <div className="space-y-3">
-        <p className="text-gray-800">
+        <p className="text-gray-800 text-base sm:text-lg md:text-xl">
           Hey James, <span className="font-semibold">Good Afternoon!</span>
         </p>
         <div className="relative">
           <input
             type="text"
             placeholder="Search dishes, restaurant"
-            className="border-2 border-gray-400 w-full p-4 pl-14 rounded-md text-black"
+            className="border-2 border-gray-400 w-full p-3 sm:p-4 pl-10 sm:pl-14 rounded-md text-black text-sm sm:text-base"
           />
-          <GrSearch size={24} className="absolute bottom-4 left-4 text-black" />
+          <GrSearch size={24} className="absolute bottom-3 sm:bottom-4 left-3 sm:left-4 text-black" />
         </div>
       </div>
     </div>
